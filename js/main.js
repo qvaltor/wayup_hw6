4af7e418944fa9c109c20e2f0173142df855dfb4
qvaltor <83728700+qvaltor@users.noreply.github.com>
2021-07-05 14:19:33 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var btn = document.getElementById('btn');
+btn.onclick = function () {
+    var text  = document.querySelector('.intro')
+    text.classList.add('red');
+    var img = document.querySelector('.desktop');
+    img.style.display = 'none';
+}
+
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	
+
+$(window).scroll(function() {
+    $('.skill').each(function(){
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+650) {
+            $(this).addClass("fadeInUp");
+        }
+    });
+});
+})
+
+$(window).scroll(function() {
+	    $('.form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
